48227fe65ad4de91340bd9e654d1ad4ea6de9176
vinaypinjani <35225431+vinaypinjani@users.noreply.github.com>
2019-08-16 00:14:23 +0500

    Initial commit

---
+++ src/com/vinaypinjani/Main.java
@@ -0,0 +1,39 @@
+package com.vinaypinjani;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println(getBucketCount(3.4, 2.1, 1.5, 2));
+        System.out.println(getBucketCount(2.75, 3.25, 2.5, 1));
+        // write your code here
+    }
+
+    public static int getBucketCount(double width, double height, double areaPerBucket, int extraBuckets) {
+        if (width <= 0 || height <= 0 || areaPerBucket <= 0 || extraBuckets < 0) {
+            return -1;
+        }
+        double area = width * height;
+        double buckets = (area / areaPerBucket);
+        return (int) Math.ceil(buckets - extraBuckets);
+    }
+
+    public static int getBucketCount(double width, double height, double areaPerBucket) {
+        if (width <= 0 || height <= 0 || areaPerBucket <= 0) {
+            return -1;
+        }
+        double area = width * height;
+        double buckets = (area / areaPerBucket);
+        return (int) Math.ceil(buckets);
+    }
+
+    public static int getBucketCount(double area, double areaPerBucket) {
+        if (area <= 0 || areaPerBucket <= 0) {
+            return -1;
+        }
+        double buckets = (area / areaPerBucket);
+        return (int) Math.ceil(buckets);
+
+    }
+
+
+}
